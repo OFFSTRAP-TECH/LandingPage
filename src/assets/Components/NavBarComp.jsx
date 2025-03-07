1408f06bb6346efe5063cf230dd5c25c13003d1b
Aryan Sardana <aryan@offstrap.com>
2025-03-07 23:56:03 +0530

    working

--- src/assets/Components/NavBarComp.jsx
+++ src/assets/Components/NavBarComp.jsx
@@ -1,26 +1,31 @@
-import Container from 'react-bootstrap/Container';
-import Nav from 'react-bootstrap/Nav';
-import Navbar from 'react-bootstrap/Navbar';
-import offstrapLogo from '../Components/logo.png';
+import Container from "react-bootstrap/Container";
+import Nav from "react-bootstrap/Nav";
+import Navbar from "react-bootstrap/Navbar";
+import offstrapLogo from "../Components/logo.png";
 
 function NavBarComp() {
-    return (
-        <Navbar bg="black" data-bs-theme="dark" t>
-            <Container className="d-flex justify-content-between align-items-center">
-                {/* Logo on the left */}
-                <Navbar.Brand href="#home">
-                    <img src={offstrapLogo} height="30px" alt="OffStrap Logo" />
-                </Navbar.Brand>
+  return (
+    <Navbar bg="black" expand="lg" data-bs-theme="dark" className="navbar-custom">
+      <Container>
+        {/* Logo on the left */}
+        <Navbar.Brand href="#home">
+          <img src={offstrapLogo} height="35px" alt="OffStrap Logo" />
+        </Navbar.Brand>
 
-                {/* Navigation links on the right */}
-                <Nav className="ms-auto">
-                    <Nav.Link href="#home">Product</Nav.Link>
-                    <Nav.Link href="#features">About</Nav.Link>
-                    <Nav.Link href="#pricing">Contact</Nav.Link>
-                </Nav>
-            </Container>
-        </Navbar>
-    );
+        {/* Toggle button for mobile */}
+        <Navbar.Toggle aria-controls="navbar-nav" />
+
+        {/* Navbar links */}
+        <Navbar.Collapse id="navbar-nav" className="justify-content-end">
+          <Nav>
+            <Nav.Link href="#home">Product</Nav.Link>
+            <Nav.Link href="#features">About</Nav.Link>
+            <Nav.Link href="#pricing">Contact</Nav.Link>
+          </Nav>
+        </Navbar.Collapse>
+      </Container>
+    </Navbar>
+  );
 }
 
 export default NavBarComp;
