acb8db152abc5af437cb5a477473aeb7b9702780
Aryan Sardana <aryan@offstrap.com>
2025-03-07 23:53:11 +0530

    working

--- src/assets/Components/Slide.jsx
+++ src/assets/Components/Slide.jsx
@@ -8,13 +8,15 @@ function Slide() {
     <section className="slide">
       <Container>
         <Row className="align-items-center">
-          <Col lg={6} className="text-lg-start text-center">
+          {/* Text Section */}
+          <Col lg={6} className="text-center text-lg-start">
             <h1 className="heading1">The Bag that Belongs to You</h1>
             <h2 className="heading2">Experience the Future of Travel</h2>
             <h2 className="heading2">Designed for Security, Connectivity, and Convenience</h2>
-            <button className="shadow__btn btn btn-primary mt-3">Pre-Order</button>
+            <button className="shadow__btn">Pre-Order</button>
           </Col>
 
+          {/* Image Section */}
           <Col lg={6} className="text-center">
             <div className="hero_image">
               <img src={bagImg} className="img-fluid" alt="Smart Bag" />
