47be7c2cadeda7396f1de78ccad6cfbc0a60dc50
Aryan Sardana <aryan@offstrap.com>
2025-03-07 23:44:00 +0530

    working

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ import { useState } from 'react'
 import NavBarComp from "./assets/Components/NavBarComp"
 import './App.css'
 import Slide from './assets/Components/Slide'
-
+import { Analytics } from "@vercel/analytics/react"
 function App() {
   
 
